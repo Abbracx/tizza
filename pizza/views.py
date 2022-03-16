352b466bf85fa4e595157bcf53b4d39d3929a898
Abbracx <tankoraphael@gmail.com>
2022-03-16 16:51:44 +0100

    Added templating and user auth steps

--- pizza/views.py
+++ pizza/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 from pizza.models import Pizza
+from django.contrib.auth.decorators import login_required
+import json
 
-# Create your views here.
+
+
+@login_required
 def index(request, pid):
-    pizza = Pizza.objects.get(id=pid)
-    return HttpResponse(pizza)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        new_pizza = Pizza.objects.create(
+            title=data['title'],
+            description=data['description'],
+            creator=request.user,
+        )
+
+        new_pizza.save()
+        return HttpResponse(new_pizza)
+
+    elif request.method == "GET":
+        pizza = Pizza.objects.get(id=pid)
+        return HttpResponse(pizza)
+    
 
 
+class GetTenPizza(View):
+    template_name = 'ten_pizza.html'
+
+    def get(self, request):
+        pizzas = Pizza.objects.order_by('?')[:10]
+        return render(request, self.template_name, {'pizzas': pizzas})
+
+    
--- user/views.py
+++ user/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from django.views import View
+
+class SignUpView(View):
+
+    template_name = 'signup.html'
+    form_class = UserCreationForm
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username, password)
+            if user:
+                login(request, user)
+                return redirect('/')
+        return render(request, self.template_name, {'form': form})
+
+    def get(self, request):
+        return render(request, self.template_name, {'form': UserCreationForm()})
